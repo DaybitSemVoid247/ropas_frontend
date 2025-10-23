7e519507576b59b0d55da021115bbb784c7e7e1e
DaybitSemVoid247 <favio5449@gmail.com>
2025-10-22 20:13:00 -0400

    feat: crear layout principal para el administrador

---
+++ src/administrator/RootAdmin.tsx
@@ -0,0 +1,18 @@
+// layouts/AdminLayout.tsx
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../components/commons/navbar";
+import { Sidebar } from "./Sidebar";
+
+export const RootAdmin = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <main className="flex-1 overflow-y-auto p-5">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+};
